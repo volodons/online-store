59f72e0f870aaa6ab106d8efd5031068941c09da
Volodymyr Donets <volodons@gmail.com>
2023-06-04 22:30:26 +0300

    Add JS to open/close sidebar

---
+++ task2_8/scripts/index.js
@@ -0,0 +1,10 @@
+const sideMenu = document.querySelector(".shop-cart");
+const buttonOpenSideMenu = document.querySelector(".shop-cart-button");
+const buttonCloseSideMenu = document.querySelector(".close-icon");
+
+let toggleSideMenu = () => {
+  sideMenu.classList.toggle("shop-cart--open");
+};
+
+buttonOpenSideMenu.addEventListener("click", toggleSideMenu);
+buttonCloseSideMenu.addEventListener("click", toggleSideMenu);
