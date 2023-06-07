58b7689c7506de6b13ccf1e714face72b976c876
Volodymyr Donets <volodons@gmail.com>
2023-06-07 10:25:08 +0300

    Add code to fetch JSON data from file

--- task2_8/scripts/index.js
+++ task2_8/scripts/index.js
@@ -8,3 +8,24 @@ let toggleSideMenu = () => {
 
 buttonOpenSideMenu.addEventListener("click", toggleSideMenu);
 buttonCloseSideMenu.addEventListener("click", toggleSideMenu);
+class DataFetcher {
+  fetchData(callback) {
+    fetch("../data/products.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Request failed with status code " + response.status);
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
+const dataFetcher = new DataFetcher();
+dataFetcher.fetchData();
