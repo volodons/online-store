21677a7b2b48ba3eabe53b093a282a1bb9aaa75a
Volodymyr Donets <volodons@gmail.com>
2023-06-13 13:04:42 +0300

    Rename product.js to Product.js

--- task2_8/scripts/index.js
+++ task2_8/scripts/index.js
@@ -1,7 +1,7 @@
 import { LocalStorageHandler } from "./localStorage.js";
 import { Renderer } from "./ui.js";
 import { DataFetcher } from "./data.js";
-import { Product } from "./products/product.js";
+import { Product } from "./products/Product.js";
 
 DataFetcher.fetchData().then((data) => {
   data.products.map((productData) => {
