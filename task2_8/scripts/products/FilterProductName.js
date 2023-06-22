c9727cc8e301259dff05369c7f7387aa93d7b356
Volodymyr Donets <volodons@gmail.com>
2023-06-22 12:12:17 +0300

    Set search action on input event

--- task2_8/scripts/products/FilterProductName.js
+++ task2_8/scripts/products/FilterProductName.js
@@ -1,11 +1,17 @@
 import { Renderer } from "../ui.js";
 import { DataFetcher } from "../data.js";
+import { debounce } from "../utils/debounce.js";
 import { urlParamsHandler } from "../utils/UrlParamsHandler.js";
 
 class FilterProductName {
   constructor(inputElement, submitElement) {
     this.inputElement = document.querySelector(inputElement);
     this.submitElement = document.querySelector(submitElement);
+    this.debouncedGetProductByName = debounce(
+      this.getProductByName.bind(this),
+      300
+    );
+    this.inputElement.addEventListener("input", this.debouncedGetProductByName);
     this.submitElement.addEventListener("submit", (event) =>
       this.onSubmit(event)
     );
@@ -15,8 +21,7 @@ class FilterProductName {
     event.preventDefault();
   }
 
-  getProductByName(event) {
-    event.preventDefault();
+  getProductByName() {
     urlParamsHandler.selectedName = this.inputElement.value.toLowerCase();
     urlParamsHandler.updateUrlParams();
     DataFetcher.fetchData().then((data) => {
