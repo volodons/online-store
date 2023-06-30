9a915c958d70f12ea86d1e12b5107d40a4f36e0a
Volodymyr Donets <volodons@gmail.com>
2023-06-30 10:56:16 +0300

    Refactor and optimize Local Storage Handler

--- task2_8/scripts/localStorage.js
+++ task2_8/scripts/localStorage.js
@@ -1,21 +1,38 @@
 class LocalStorageHandler {
   static getItems(key) {
-    const localStorageKey = localStorage.key(key);
-    const localStorageValueJSON = localStorage.getItem(localStorageKey);
-    const localStorageValue = JSON.parse(localStorageValueJSON);
-    return localStorageValue;
+    if (key) {
+      const localStorageKey = localStorage.key(key.toString());
+      const localStorageValueJSON = localStorage.getItem(localStorageKey);
+      const localStorageValue = JSON.parse(localStorageValueJSON);
+      return localStorageValue;
+    }
+    throw new Error("Missing key parameter");
   }
 
   static setItems(localStorageKey, localStorageValue) {
-    localStorage.setItem(
-      JSON.stringify(localStorageKey),
-      JSON.stringify(localStorageValue)
-    );
+    if (localStorageKey && localStorageValue) {
+      try {
+        localStorage.setItem(
+          localStorageKey.toString(),
+          JSON.stringify(localStorageValue)
+        );
+      } catch (error) {
+        console.error("Failed to set item in localStorage: ", error);
+      }
+    }
   }
 
   static removeItems(localStorageKey) {
-    const localStorageKeyValue = localStorage.key(localStorageKey);
-    localStorage.removeItem(localStorageKeyValue);
+    if (localStorageKey) {
+      const localStorageKeyValue = localStorage.key(localStorageKey.toString());
+      if (localStorageKeyValue) {
+        try {
+          localStorage.removeItem(localStorageKeyValue);
+        } catch (error) {
+          console.error("Failed to remove item from localStorage: ", error);
+        }
+      }
+    }
   }
 }
 
