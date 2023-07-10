0317f002450ae9dfa29a4af637b61cc8e6356b6e
Volodymyr Donets <volodons@gmail.com>
2023-07-10 10:36:46 +0300

    Remove redundant product.js file

--- task2_8/scripts/index.js
+++ task2_8/scripts/index.js
@@ -2,15 +2,11 @@ import { LocalStorageHandler } from "./localStorage.js";
 import { Renderer } from "./ui.js";
 import { DataFetcher } from "./data.js";
 import { shoppingCart } from "./cart.js";
-import { Product } from "./products/Product.js";
 import { urlParamsHandler } from "./utils/UrlParamsHandler.js";
 
 function initializeApp() {
   try {
     DataFetcher.fetchData().then((data) => {
-      data.products.map((product) => {
-        Product.createProduct(product);
-      });
       Renderer.renderProducts(data.products);
     });
     Renderer.renderItems(LocalStorageHandler.getLocalStorageItem("cart"));
